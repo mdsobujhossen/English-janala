cf6b5daf291dc4016822d28e6ba3f17d88ac2ae4
mdsobujhossen <itmdsobuj@gmail.com>
2026-03-04 07:41:12 +0600

    added words section and recap the till works

--- script/index.js
+++ script/index.js
@@ -9,7 +9,6 @@ const levelWords = (lessonsLevel) => {
 
 
 const displayLevelWords = (words) => {
-    console.log(words);
     const wordsContainer = document.getElementById("word-container")
     wordsContainer.innerHTML = ""
 
@@ -25,12 +24,15 @@ const displayLevelWords = (words) => {
 
     words.forEach(word => {
         const wordDiv = document.createElement("div")
+        wordDiv.className = "bg-base-100 rounded-md shadow space-y-4 p-10 h-full text-center"
         wordDiv.innerHTML = `
-         <div class="bg-base-100 rounded-md shadow space-y-6 py-10 px-4 h-full text-center">
-                <h3 class="text-2xl font-semibold leading-6">${word.word}</h3>
-                <p class="text-xl font-medium leading-6">Meaning /Pronounciation</p>
-                <p class="font-bangla text-[28px] font-semibold text-[#2d2d2e]">"${word.meaning} /  ${word.pronunciation}"</p>
-            </div>
+                <h3 class="text-2xl font-medium leading-6">${word.word}</h3>
+                <p class="text-xl font-normal leading-6">Meaning /Pronounciation</p>
+                <p class="font-bangla text-2xl font-medium text-[#2d2d2e]">"${word.meaning} /  ${word.pronunciation}"</p>
+                <div class="flex justify-between items-center pt-5">
+                    <button class="btn bg-[#1A91FF10]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[#1A91FF10]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
         `
         wordsContainer.append(wordDiv)
     });
